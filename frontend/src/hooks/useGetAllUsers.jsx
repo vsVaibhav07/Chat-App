82884c1bc2cff92b420e45edc44989b989cd6dc2
Vaibhav Shukla <vaibhavshuklamdi@gmail.com>
2025-07-19 12:58:23 +0530

    updated GetAllUsers

--- frontend/src/hooks/useGetAllUsers.jsx
+++ frontend/src/hooks/useGetAllUsers.jsx
@@ -1,15 +1,16 @@
 import axios from "axios";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setOtherUsers } from "../redux/otherUsersSlice";
 
 const useFetchUsers = () => {
   const dispatch = useDispatch();
+  const authUser=useSelector((state) => state.user.authUser);
 
   useEffect(() => {
     const getAllUsers = async () => {
       try {
-        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/v1/user`, {
+        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/v1/${authUser.id}`, {
           withCredentials: true,
         });
         if (response.data.success) {
